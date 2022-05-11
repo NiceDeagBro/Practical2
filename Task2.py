f0d39dc96b14dc1eb39977d3625fb6cad02d81d2
Many Names <m@ghost.local>
2022-05-11 16:30:02 +0300

    first push

--- Task2.py
+++ Task2.py
@@ -1,25 +1,22 @@
+# TASK 2.2
+
 from dataclasses import dataclass, field                        # import of additional modules dataclass and field, from library dataclasses.
 
-
 @dataclass                                                      # declaring that this class is going to be dataclass
-class Item:                                                     # class Item describes three item characteristics – name, count and price.
+class Item:                                                     # class Item describes three item chracteristics – name, count and price.
     dict = {}                                                   # declaration of the dictionary.
     name: str                                                   # declaration of the name variable.
     count: int = field(default=1)                               # declaration of the count variable.
-    price: float = field(default=10.0)
-
-    # declaration of the price variable.
+    price: float = field(default=10.0)                          # declaration of the price variable.
     def get_total_price(self):                                  # method for calculating total price of the item (multiplies count on price).
         return f"Total price: {self.count * self.price}"        # returns a string containing value of the calculation.
-
-    def full_info(self):                                        # method for printing all information about the item (name, count, price, total price).
+    def full_info(self):                                        # method for prinitng all information about the item (name, count, price, total price).
         return f"\nName: {self.name},\nCount: {self.count},\nPrice: {self.price},\nTotal price: {self.get_total_price()}"       # returns a string with full information of the item.
-
-    def to_dict(self):                                          # method for insertion of the values into the dictionary
+    def to_dict(self):                                          # method for insertation of the values into the dictionary
         self.dict['name'] = self.name                           # key 'name' equals name of the item
         self.dict['count'] = self.count                         # key 'name' count name of the count
         self.dict['price'] = self.price                         # key 'name' price name of the price
-        return self.dict                                        # Return keys and its values
+        return self.dict                                        # returns keys and its values
 
 
 i1 = Item("Carrots")                # creating a variable and assigning value for the first item of class Item()
@@ -30,3 +27,5 @@ item_list = [i1, i2, i3]            # adding new variables to a list
 for i in item_list:                 # for each variable in the list...
     print(i.full_info())            # ...use method full_info()
     print(i.to_dict())              # ...use method to_dict()
+
+
--- Task3.py
+++ Task3.py
@@ -1,48 +1,46 @@
-from dataclasses import dataclass, field
+# TASK 2.3
+from dataclasses import dataclass, field                        # import of additional modules dataclass and field, from library dataclasses.
 
-
-@dataclass
-class Item:
-    dict = {}
-    name: str
-    count: int = field(default=1)
-    price: float = field(default=10.0)
-
-    def get_total_price(self):
-        return f"Total price: {self.count * self.price}"
-
-    def full_info(self):
-        return f"\nName: {self.name},\nCount: {self.count},\nPrice: {self.price},\nTotal price: {self.get_total_price()}"
-
-    def to_dict(self):
-        self.dict['name'] = self.name
-        self.dict['count'] = self.count
-        self.dict['price'] = self.price
-        return self.dict
-
+@dataclass                                                      # declaring that this class is going to be dataclass
+class Item:                                                     # class Item describes three item chracteristics – name, count and price.
+    dict = {}                                                   # declaration of the dictionary.
+    name: str                                                   # declaration of the name variable.
+    count: int = field(default=1)                               # declaration of the count variable.
+    price: float = field(default=10.0)                          # declaration of the price variable.
+    def get_total_price(self):                                  # method for calculating total price of the item (multiplies count on price).
+        return f"Total price: {self.count * self.price}"        # returns a string containing value of the calculation.
+    def full_info(self):                                        # method for prinitng all information about the item (name, count, price, total price).
+        return f"\nName: {self.name},\nCount: {self.count},\nPrice: {self.price},\nTotal price: {self.get_total_price()}"       # returns a string with full information of the item.
+    def to_dict(self):                                          # method for insertation of the values into the dictionary
+        self.dict['name'] = self.name                           # key 'name' equals name of the item
+        self.dict['count'] = self.count                         # key 'name' count name of the count
+        self.dict['price'] = self.price                         # key 'name' price name of the price
+        return self.dict                                        # returns keys and its values
 
 @dataclass
 class Food(Item):
-    def full_info(self):
-        return f"\nFood:\n\tName: {self.name},\n\tCount: {self.count},\n\tPrice: {self.price},\n\tTotal price: {self.get_total_price()}"
+    def full_info(self):                                        # method for prinitng all information about the item (name, count, price, total price).
+        return f"\nFood:\n\tName: {self.name},\n\tCount: {self.count},\n\tPrice: {self.price},\n\tTotal price: {self.get_total_price()}"       # returns a string with full information of the item.
 
 
 class Drink(Item):
-    def full_info(self):
-        return f"Drink:\n\tName: {self.name},\n\tCount: {self.count},\n\tPrice: {self.price},\n\tTotal price: {self.get_total_price()}"
+    def full_info(self):                                        # method for prinitng all information about the item (name, count, price, total price).
+        return f"Drink:\n\tName: {self.name},\n\tCount: {self.count},\n\tPrice: {self.price},\n\tTotal price: {self.get_total_price()}"       # returns a string with full information of the item.
 
+f1 = Food("Bread", 2, price=0.5)    # creating a variable and assigning values for the first item of class Food()
+f2 = Food("Bread", 2, price=0.5)    # creating a variable and assigning values for the second item of class Food()
 
-f1 = Food("Bread", 2, price=0.5)
-f2 = Food("Bread", 2, price=0.5)
-
-d1 = Drink("Bread", 2, price=0.5)
-d2 = Drink("Bread", 2, price=0.5)
+d1 = Drink("Bread", 2, price=0.5)    # creating a variable and assigning values for the first item of class Drinks()
+d2 = Drink("Bread", 2, price=0.5)    # creating a variable and assigning values for the second item of class Drinks()
 
     
-food_list = [f1, f2]
-for f in food_list:
-    print(f.full_info())
+food_list = [f1, f2]            # adding new variables to a list
+for f in food_list:                 # for each variable in the list...
+    print(f.full_info())            # ...use method full_info()
 
-drink_list = [d1, d2]
-for d in drink_list:
-    print(d.full_info())
+drink_list = [d1, d2]            # adding new variables to a list
+for d in drink_list:                 # for each variable in the list...
+    print(d.full_info())            # ...use method full_info()
+
+
+
