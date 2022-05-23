9dad0dcafc852d26a184ceb310f1b84d63f53bcd
ghost <sevastian.zare@knf.stud.vu.lt>
2022-05-23 08:53:36 +0300

    decorator still undone

--- Other/Task4.py
+++ Other/Task4.py
@@ -1,12 +1,53 @@
-import Task2
+# Decorator itself
+def process_list(original_list, x):
+    buffer_list = []
+    new_list = []
+    for lists in original_list:
+        buffer_list.append(lists)
+        print(buffer_list)
+    for i in buffer_list:
+        i = i - x
+        new_list.append(i)
 
-def decorator(func, x):
-    def func_arg(arg):
-        result = func(arg)
-        for item in arg:
-            item = item - x
-        return result
-    return(func_arg)
+    def decorator(original_func):
+        return original_func(new_list)
+    return decorator
 
-# print decorated output of function "filter_list" of Task2 
-print(decorator(Task2.filter_list([1, 10, 34, 110, 400, 30, 20, 100, 101, 9, 11, 10]), 10))
+# Task 2
+
+
+def filter_list(one_d_list):
+	filtered = []
+
+	for item in one_d_list:
+		if 100 >= item >= 10:
+			filtered.append(item)
+
+	mean_value = sum(filtered) / len(filtered)
+	min_value = min(filtered)
+	max_value = max(filtered)
+	sum_of_values = sum(filtered)
+	return mean_value, min_value, max_value, sum_of_values
+
+
+def filter_two_d_list(two_d_list):
+
+	for obj in two_d_list:
+		filtered = filter_list(obj)
+		print(filtered)
+
+
+# Variables
+filter_two_d_list([
+    [1, 10, 34, 110, 400, 30, 20],
+    [-5, -10, 55, 120, 30],
+    [2, 67, 23, 78, 200],
+])
+
+# Implementation of decorator
+decorated = process_list(filter_two_d_list, 10)
+decorated([
+    [1, 10, 34, 110, 400, 30, 20],
+    [-5, -10, 55, 120, 30],
+    [2, 67, 23, 78, 200],
+])
